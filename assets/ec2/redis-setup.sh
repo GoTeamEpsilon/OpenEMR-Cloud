fa20d3ed4b01b88b50c8d71392b382a312d5e58b
Matthew Vita <matthewvita@live.com>
2017-06-07 21:18:01 -0400

    Correction to redis config

--- assets/ec2/redis-setup.sh
+++ assets/ec2/redis-setup.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update -y
 sudo apt-get install -y redis-server
-sudo sed -i 's/bind\ 127.0.0.1/;bind\ 127.0.0.1/' /etc/redis/redis.conf
+sudo sed -i 's/bind\ 127.0.0.1/\#bind\ 127.0.0.1/' /etc/redis/redis.conf
